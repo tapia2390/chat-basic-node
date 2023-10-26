33b31b9136716aebb6b99c4e3868860697c77ba7
tapia2390 <tapia2390@gmail.com>
2023-10-26 16:55:14 -0500

    chat node js

---
+++ index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const socketIO = require('socket.io');
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+        
+// start the server
+const server = app.listen(app.get('port'), () => {
+  console.log('server on port', app.get('port'));
+});
+
+
+const io = socketIO(server);
+io.on('connection', (socket) => {
+    console.log('new connection', socket.id);
+    });
